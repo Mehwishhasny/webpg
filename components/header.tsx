6605869a76f1ad226809304c804d0cbd91920a0d
Mehwish Hasny <mehwish.hasny@gmail.com>
2024-10-12 21:25:07 +0500

    commit added

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,6 @@ export function Header() {
             <h1 className="text-black font-bold text-xl cursor-pointer"><Link href="/"> <b>WRAFTY</b> creations</Link></h1>
         
             <ul className="flex text-black font-bold sm:text-xl sm:space-x-8 text-lg space-x-4 sm:mr-4">
-                <li className="hover:text-gray-600"><Link href="/"> Home</Link></li>
                 <li className="hover:text-gray-600"><Link href="/about">About</Link></li>
                 <li className="hover:text-gray-600"><Link href="/portfolio">Portfolio</Link></li>
                 <li className="hover:text-gray-600"><Link href="/contact">Contact</Link></li>
